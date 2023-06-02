9544ddc633e383465ee137608e27eacd6802fa65
Brian Kim <71394874+briankim-pitt@users.noreply.github.com>
2023-06-02 14:21:40 -0400

    update main.py

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ import discord
 from disnake.ext import commands
 
 bot = commands.Bot(
-
+    command_prefix="!",
 )
+    
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 
 @bot.slash_command(description="Responds with 'World'")
 async def hello(inter):
     await inter.response.send_message("World")
+
+bot.run('MTExNDI0OTY1NDc5MjYyNjI3Nw.GI0y-I.Fu1t-Z2Y7MJArKCsHKSHRqrsXpMuLZnOyiagac')
