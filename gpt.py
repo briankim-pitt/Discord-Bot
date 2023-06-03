9a07aed64c96404f565489f8b4eb69bd112c9e56
Brian Kim <71394874+briankim-pitt@users.noreply.github.com>
2023-06-02 21:45:31 -0400

    update main and gpt

---
+++ gpt.py
@@ -0,0 +1,125 @@
+import openai
+import credentials 
+import json
+import requests
+import mysql.connector
+from mysql.connector import Error
+
+
+openai.api_key = credentials.openai_key
+
+
+# def getChat(guild):
+#     chat = []
+#     try:
+#         connection = mysql.connector.connect(host="localhost",
+#                                              database="gpt_bot",
+#                                              user="root",
+#                                              password="root")
+#         cursor = connection.cursor()
+        
+#     except mysql.connector.Error as error: 
+#         print("Failed to create table: {}".format(error))
+
+
+#     return chat
+
+
+# def saveQuestion(guild, question):
+#     try:
+#         connection = mysql.connector.connect(host="localhost",
+#                                              database="gpt_bot",
+#                                              user="root",
+#                                              password="root")
+#         mySql_Create_Table_Query = """CREATE TABLE DB_""" + str(guild) + """ (
+#         Id int(11) NOT NULL AUTO_INCREMENT,
+#         User varchar(250) NOT NULL,
+#         Message varchar(5000) NOT NULL,
+#         PRIMARY KEY (Id)) """
+
+#         cursor = connection.cursor()
+#         result = cursor.execute(mySql_Create_Table_Query)
+#         print("Table created")
+
+#     except mysql.connector.Error as error:
+#         print("Failed to create table: {}".format(error))
+
+#     finally:
+#         if connection.is_connected():
+#             table = "DB_" + str(guild)
+#             mySql_Insert_Row_Query = "INSERT INTO " + table + " (question) VALUES (%s)"
+#             message = {"role": "user", "content": question}
+#             mySql_Insert_Row_values = (message)
+#             cursor.execute(mySql_Insert_Row_Query, mySql_Insert_Row_values)
+
+            
+
+#             print("question stored in db")
+
+#             cursor.close()
+
+#             connection.close()
+
+# def saveAnswer(guild, answer):
+#     try:
+#         connection = mysql.connector.connect(host="localhost",
+#                                              database="gpt_bot",
+#                                              user="root",
+#                                              password="root")
+#         cursor = connection.cursor()
+
+#     except mysql.connector.Error as error:
+#         print("Failed to create table: {}".format(error))
+#     finally:
+#         if connection.is_connected():
+#             table = "DB_" + str(guild)
+#             mySql_Insert_Row_Query = "INSERT INTO " + table + " (answer) VALUES (%s)"
+#             mySql_Insert_Row_values = (answer)
+#             cursor.execute(mySql_Insert_Row_Query, mySql_Insert_Row_values)
+    
+#     print("question stored in db")
+
+#     cursor.close()
+#     connection.close()
+
+chat=[]
+
+def romanize(text):
+    response = openai.Completion.create(
+        model="text-curie-001",
+        prompt=f"Romanize this: {text}",
+        temperature=0.3,
+        max_tokens=200,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    print(response['choices'][0]['text'].lstrip("\n"))
+    return response['choices'][0]['text'].lstrip("\n")
+
+def translate(text, targetLang):
+    response = openai.Completion.create(
+        model="text-curie-001",
+        prompt=f"Translate this into {targetLang}: {text}",
+        temperature=0.3,
+        max_tokens=200,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    print(response['choices'][0]['text'].lstrip("\n"))
+    return response['choices'][0]['text'].lstrip("\n")
+
+def ask(guild, question):
+    # saveQuestion(guild, answer)
+    # chat = getChat(guild)
+    chat.append({"role": "user", "content": question})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat
+    )
+    answer = response['choices'][0]['message']
+    chat.append(answer)
+    # saveAnswer(guild, answer)
+    return response['choices'][0]['message']['content']
+
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import asyncio
 import disnake
 import discord
 import credentials
+import gpt
+
 
 from disnake.ext import commands
 
+users = {}
+
+#settings
+autotranslate = False
+romanize = False
+targetLang = "English"
+
 bot = commands.Bot(
     command_prefix="!",
+    intents=disnake.Intents.all()
 )
     
 @bot.event
@@ -15,14 +26,76 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
-        return
+        return    
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if bot.user.mentioned_in(message):
+        guild = message.guild.id
+        print(f"guild: {guild}")
+        text = message.content.split(' ', 1)[1]
+        print(text)
+        async with message.channel.typing():
+            await asyncio.sleep(0.1)
+            response = gpt.ask(guild, text)
+            await message.channel.send(response)
+    else:
+        if romanize:
+            response = gpt.romanize(message.content)
+            await message.channel.send("Romanized: ||" + response + "||")
+
+        if users.get(message.author, False):
+            global targetLang
+            response = gpt.translate(message.content, targetLang)
+            await message.channel.send("Translated: ||" + response + "||")
+        
+        
+        
 
 
 @bot.slash_command(description="Responds with 'World'")
 async def hello(inter):
     await inter.response.send_message("World")
 
-bot.run(credentials.token)
+@bot.slash_command(description="Toggle Auto-Translate Globally")
+async def toggle_at_global(inter):
+    global autotranslate
+    autotranslate = not autotranslate
+    if autotranslate:
+        await inter.response.send_message("Auto-translate is now on")
+    else:
+        await inter.response.send_message("Auto-translate is now off")
+
+@bot.slash_command(description="Toggle Auto-Translate")
+async def toggle_at(inter):
+    users[inter.user] = not users.get(inter.user, False)
+    if users[inter.user]:
+        await inter.response.send_message("Auto-translate is now on")
+    else:
+        await inter.response.send_message("Auto-translate is now off")
+
+@bot.slash_command(description="Toggle Romanize")
+async def toggle_ro(inter):
+    global romanize
+    romanize = not romanize
+    if romanize:
+        await inter.response.send_message("Auto-romanization is now on")
+    else:
+        await inter.response.send_message("Auto-romanization is now off")
+
+@bot.slash_command(description="Set target language")
+async def set_target_lang(inter, language):
+    global targetLang
+    targetLang = language
+    await inter.response.send_message("Target language has been set to " + language)     
+
+
+@bot.command(description="Ask GPT")
+async def ask(ctx, arg):
+    await ctx.channel.send("hi")
+    response = gpt.ask(arg)
+    await ctx.channel.send(response)
+
+@bot.command(description="Say Hi")
+async def hi(ctx: commands.Context):
+    print("hello")
+    await ctx.send('hello')
+bot.run(credentials.disc_token)
